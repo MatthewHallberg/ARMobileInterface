1a703ff78d818f934657b3714dd7f0b3d721698c
Matthew Hallberg <matthewhallberg@gmail.com>
2019-01-17 00:24:26 -0500

    first commit

---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceController : MonoBehaviour {
+    public static InterfaceController Instance { get; private set; }
+
+    void Awake() {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageController : MonoBehaviour {
+    public static MessageController Instance { get; private set; }
+
+    void Awake() {
+        Instance = this;
+
+    }
+
+
+}
